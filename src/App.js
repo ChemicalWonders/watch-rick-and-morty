b39c3adc58e74575f740d06ce2e241bd5a546f8a
Kevin Chan <kchan039@gmail.com>
2019-12-22 20:54:40 -0800

    Cleaned up titles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactPlayer from 'react-player';
 
 import './App.css';
 
 function App() {
   return (
+    <React.Fragment>
     <div className="App">
-      Rick and Morty Live Stream
       <header className="App-header">
-        <ReactPlayer url="https://adultswim-vodlive.cdn.turner.com/live/rick-and-morty/stream.m3u8" playing controls/>
+        <p>Rick and Morty</p>
+        <ReactPlayer url="https://adultswim-vodlive.cdn.turner.com/live/rick-and-morty/stream.m3u8" playing controls />
+        <p>
+          Created by <a href="https://github.com/ChemicalWonders">@ChemicalWonders</a>
+        </p>
+
       </header>
     </div>
+    </React.Fragment>
   );
 }
 
