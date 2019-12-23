3c33eb59e2f8a43cd81ab80ef87386883c1ba81e
Kevin Chan <kchan039@gmail.com>
2019-12-22 20:30:31 -0800

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      Rick and Morty Live Stream
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ReactPlayer url="https://adultswim-vodlive.cdn.turner.com/live/rick-and-morty/stream.m3u8" playing controls/>
       </header>
     </div>
   );
