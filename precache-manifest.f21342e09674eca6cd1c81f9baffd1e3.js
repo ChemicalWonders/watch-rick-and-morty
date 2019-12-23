189e932b21d81b708861c13c4eb3c28dc24f65ce
Kevin Chan <kchan039@gmail.com>
2019-12-22 20:36:52 -0800

    Updates

---
+++ precache-manifest.f21342e09674eca6cd1c81f9baffd1e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7347dc2f2f5a62a78d405f22b76b45e4",
+    "url": "/watch-rick-and-morty/index.html"
+  },
+  {
+    "revision": "e4580297fb6a73bb9154",
+    "url": "/watch-rick-and-morty/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "78fc676c943bcbe6819d",
+    "url": "/watch-rick-and-morty/static/js/2.d908e115.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/watch-rick-and-morty/static/js/2.d908e115.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4580297fb6a73bb9154",
+    "url": "/watch-rick-and-morty/static/js/main.f96de2f2.chunk.js"
+  },
+  {
+    "revision": "6684bdebff234a991860",
+    "url": "/watch-rick-and-morty/static/js/runtime-main.5bdcac87.js"
+  }
+]);
